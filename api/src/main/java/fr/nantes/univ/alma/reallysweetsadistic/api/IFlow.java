04e56dc4250558f0bba5c5793b7247a84838d216
Mael Barbin <bn.mael@gmail.com>
2013-11-26 11:55:54 +0100

    - Complete the IUserManager interface - Add the IFlow interface

---
+++ api/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/api/IFlow.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package fr.nantes.univ.alma.reallysweetsadistic.api;
+
+import java.util.Date;
+
+/**
+ * @author Maël
+ *
+ */
+public interface IFlow {
+	
+	/**
+	 * Get the address of this Flow.
+	 * @return the address of this Flow.
+	 */
+	public String getAddress();
+	
+	/**
+	 * Get the title of this Flow.
+	 * @return the title of this Flow.
+	 */
+	public String getTitle();
+	
+	/**
+	 * Get the content of this Flow.
+	 * @return the content of this Flow.
+	 */
+	public String getContent();
+	
+	/**
+	 * Get the last update for this Flow.
+	 * @return the last update for this Flow.
+	 */
+	public Date getLastUpdate();
+	
+	/**
+	 * Set the content of this Flow.
+	 */
+	public void setContent();
+	
+}
--- api/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/api/IUserManager.java
+++ api/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/api/IUserManager.java
@@ -8,5 +8,34 @@ package fr.nantes.univ.alma.reallysweetsadistic.api;
  *
  */
 public interface IUserManager {
-
+	
+	/**
+	 * User authentication with user name and password.
+	 * @param userName the user name of the user who wants to be authenticated.
+	 * @param password associated with the userName.
+	 */
+	public void authentication(String userName, String password);
+	
+	/**
+	 * Register a new user. Storing userName and associated password.
+	 * @param userName of the new user.
+	 * @param password of the new user.
+	 */
+	public void register(String userName, String password);
+	
+	/**
+	 * A user subscribe to a Flow.
+	 * @param userName of the user who wants to subscribe to a flow.
+	 * @param flow the flow that the user wants to subscribe.
+	 * @return a boolean indicate if subscribe operation is OK or KO.
+	 */
+	public boolean subscribe(String userName, IFlow flow);
+	
+	/**
+	 * A user unsubscribe to a Flow.
+	 * @param userName of the user who wants to subscribe to a flow.
+	 * @param flow the flow that the user wants to subscribe.
+	 * @return a boolean indicate if subscribe operation is OK or KO.
+	 */
+	public boolean unsubscribe(String userName, IFlow flow);
 }
