2d9adc9a36d51bdb0532d400f32e7ffe64b300cd
Mael Barbin <bn.mael@gmail.com>
2013-11-29 15:59:52 +0100

    Start REST for reader

--- services/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/services/impl/Reader.java
+++ services/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/services/impl/Reader.java
@@ -6,8 +6,12 @@ package fr.nantes.univ.alma.reallysweetsadistic.services.impl;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 import fr.nantes.univ.alma.reallysweetsadistic.api.IFlow;
 import fr.nantes.univ.alma.reallysweetsadistic.api.IFlowManager;
+import fr.nantes.univ.alma.reallysweetsadistic.api.impl.FlowManager;
 import fr.nantes.univ.alma.reallysweetsadistic.services.IReader;
 import fr.nantes.univ.alma.reallysweetsadistic.services.listener.FlowListenerThread;
 
@@ -23,6 +27,7 @@ import fr.nantes.univ.alma.reallysweetsadistic.services.listener.FlowListenerThr
  * @author nemo
  *
  */
+@Path("Reader")
 public class Reader implements IReader {
 	private FlowListenerThread listener;
 	private List<String> urls;
@@ -32,10 +37,12 @@ public class Reader implements IReader {
 	 * Constructor for an user
 	 * @param userName {@link String} - The user name
 	 */
-	public Reader(IFlowManager flowManager) {
+	public Reader() {
 		this.urls = new LinkedList<String>();
 		this.listener = new FlowListenerThread(this);
-		this.flowManager = flowManager;
+		this.flowManager = new FlowManager();
+		
+		this.init();
 	}
 	
 	@Override
@@ -48,7 +55,15 @@ public class Reader implements IReader {
 		return null;
 	};
 	
+	/**
+	 * Initialize all basics RSS Feed.
+	 */
+	private void init(){
+		this.urls.add("http://magazine.developpez.com/magazine-rss.xml");
+	}
+	
 	@Override
+	@GET
 	public List<String> getFlows() {
 		return this.urls;
 	}
