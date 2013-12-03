a94f5a7468fd5eb47f3cbbe39c4914c0dd04487c
bmael <bn.mael@gmail.com>
2013-12-03 10:39:04 +0100

    Authentication should work for client

--- client/src/main/java/main/Client.java
+++ client/src/main/java/main/Client.java
@@ -1,25 +1,93 @@
 package main;
 
-import fr.nantes.univ.alma.reallysweetsadistic.api.impl.FlowManager;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 public class Client {
 	
-	public Client() {
-		System.out.println("[Client] Salut c'est moi!");
-	}
-	
-	public void sayCoucou() {
-		System.out.println("[Client] Coucou! Tu veux voir mon code?");
-	}
-	
-	public void list() {
+	private static List<NameValuePair> initUserPanel(){
+		List<NameValuePair> params = null;
 		
+		JPanel panel = new JPanel(new SpringLayout());
+		
+		JLabel userNameLabel = new JLabel("User Name:", JLabel.TRAILING);
+		JTextField userName = new JTextField(10);
+		userNameLabel.setLabelFor(userName);
+		panel.add(userNameLabel);
+		panel.add(userName);
+		
+		JLabel passwordLabel = new JLabel("Password:", JLabel.TRAILING);
+		JPasswordField password = new JPasswordField(10);
+		passwordLabel.setLabelFor(password);
+		panel.add(passwordLabel);
+		panel.add(password);
+		
+		//Lay out the panel.
+		SpringUtilities.makeCompactGrid(panel,
+		                                2, 2, //rows, cols
+		                                6, 6,        //initX, initY
+		                                6, 6);       //xPad, yPad
+
+		int option = JOptionPane.showOptionDialog(null, panel, "User information", JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null, null, JOptionPane.OK_OPTION);
+		if(option == JOptionPane.OK_OPTION && !userName.getText().isEmpty() && !new String(password.getPassword()).isEmpty())
+		{
+	        params = new ArrayList<NameValuePair>();
+	        params.add(new BasicNameValuePair("userName", userName.getText()));
+			params.add(new BasicNameValuePair("password", new String(password.getPassword())));
+			System.out.println("userName: " + userName.getText());
+			System.out.println("password: " + new String(password.getPassword()));
+		}
+		
+		return params;
 	}
+
+	protected static String getContextAsString(HttpResponse response) throws IOException {
+
+        StringWriter writer = new StringWriter();
+        InputStream inputStream = response.getEntity().getContent();
+        try {
+            IOUtils.copy(inputStream, writer, "UTF-8");
+        } finally {
+            inputStream.close();
+        }
+        return writer.toString();
+    }
 	
-	public static void main(String[] args) {
-		FlowManager flowManager=new FlowManager();
-		Client c=new Client();
-		c.sayCoucou();
+	public static void main(String[] args) throws Exception {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+
+		// Authentication request
+		HttpPost authentication = new HttpPost("http://localhost:8080/reader-services-0.1-SNAPSHOT/service/authentifier/authentication");
+		
+		List<NameValuePair> params = initUserPanel();
+		
+		UrlEncodedFormEntity encodedFormEntity = new UrlEncodedFormEntity(params, "UTF-8");
+		authentication.setEntity(encodedFormEntity);
+        CloseableHttpResponse response = httpClient.execute(authentication);
+
+        System.out.println(response);
+        System.out.println(getContextAsString(response));
 	}
-	
+
 }
--- services/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/services/impl/Authentifier.java
+++ services/src/main/java/fr/nantes/univ/alma/reallysweetsadistic/services/impl/Authentifier.java
@@ -6,6 +6,12 @@ package fr.nantes.univ.alma.reallysweetsadistic.services.impl;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import fr.nantes.univ.alma.reallysweetsadistic.api.IUser;
 import fr.nantes.univ.alma.reallysweetsadistic.api.IUserManager;
 import fr.nantes.univ.alma.reallysweetsadistic.api.impl.UserManager;
@@ -14,6 +20,8 @@ import fr.nantes.univ.alma.reallysweetsadistic.api.impl.UserManager;
  * @author Maël
  * 
  */
+@Controller
+@RequestMapping("authentifier")
 public class Authentifier {
 	private List<IUser> connected;
 	private IUserManager userManager;
@@ -32,7 +40,9 @@ public class Authentifier {
 	 * @return {@link Boolean boolean} - <code>true</code> if the authentication
 	 *         was correct, <code>false</code> otherwise
 	 */
-	public boolean auth(String userName, String password) {
+    @RequestMapping(value="authentication", method = RequestMethod.POST)
+    @ResponseBody
+	public boolean auth(@RequestParam(value = "userName") String userName, @RequestParam(value = "password") String password) {
 
 		IUser user = this.userManager.authentication(userName,
 				UserManager.getMD5Encryption(password));
@@ -46,7 +56,9 @@ public class Authentifier {
 	 * @param userName
 	 * @return
 	 */
-	public boolean disconnect(String userName) {
+    @RequestMapping(value="disconnection", method = RequestMethod.POST)
+    @ResponseBody
+	public boolean disconnect(@RequestParam(value = "userName") String userName) {
 		for (int i = this.connected.size(); i > 0; i--) {
 			if (this.connected.get(i).getUserName() == userName) {
 				return this.connected.remove(this.connected.get(i));
